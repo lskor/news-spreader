c950f53281bdd4aa5eed6b321e128db00d8b02bc
lskor <glory.web3@gmail.com>
2023-05-19 13:55:12 +0300

    - sending hardcoded text to default channel

--- bot/src/main/scala/BotApp.scala
+++ bot/src/main/scala/BotApp.scala
@@ -1,29 +1,35 @@
 import canoe.api._
+import canoe.models.Channel
 import canoe.syntax._
-import cats.effect.{IO, IOApp}
-import cats.syntax.all._
+import cats.effect.{ExitCode, IO, IOApp}
 import fs2.Stream
+
+import scala.concurrent.duration._
 
 object BotApp extends IOApp.Simple {
 
 	val token: String = "token"
 
-	override def run: IO[Unit] =
-		Stream
-			.resource(TelegramClient[IO](token))
-			.flatMap(implicit client => Bot.polling[IO].follow(greetings))
-			.compile
-			.drain
+	val chat = Channel(
+		id = -1001795303736L,
+		title = Some("Important News"),
+		username = Some("really_important_news"))
 
-	def greetings[F[_] : TelegramClient]: Scenario[F, Unit] = {
+	val testPost =
+		"The gala concert of the Tallinn Talent music competition is open to the public \n\n" +
+		"On Sunday 21 May at 2 pm, the festive closing concert of the Tallinn Talent 2023 youth music competition will" +
+		" be held at the Estonia Concert Hall, where its initiator and patron Mayor Mihhail Kõlvart will acknowledge " +
+		"the winners and present them with city scholarships. The free gala concert is open to the public.\n\n " +
+		"16. May 2023 "
 
-		val post: String = "Вниманию жителей дома по ул. Лескова, 23! " +
-			"Отключение горячей воды в связи с проведением работ по подготовке дома к отопительному сезону " +
-			"15 мая 2023"
-
-		for {
-			chat <- Scenario.expect(command("start").chat)
-			_ <- Scenario.eval(chat.send(post))
-		} yield ()
+	override def run: IO[Unit] = {
+		TelegramClient[IO](token).use(implicit client =>
+			for {
+				_ <- Stream.awakeEvery[IO](7.seconds)
+						.evalMap(_ => chat.send(testPost))
+						.compile
+						.drain
+			} yield ()
+		).as(ExitCode.Success)
 	}
 }
