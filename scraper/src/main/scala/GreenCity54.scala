e49651be5a08663ce30399a9bbdd7d18111fc90f
lskor <glory.web3@gmail.com>
2023-05-19 18:49:37 +0300

    - fixed format GreenCity54

--- scraper/src/main/scala/GreenCity54.scala
+++ scraper/src/main/scala/GreenCity54.scala
@@ -4,21 +4,20 @@ import org.jsoup.nodes.Document
 
 object GreenCity54
 {
-
 	private val url = "http://greencity54.ru/press/oazis54/"
 
 	def getNews(number: Long): NewsDTO =
 	{
-		val doc = Jsoup.connect(s"$url/$number/").get()
+		val doc = Jsoup.connect(s"$url$number/").get()
 
 		val outputSettings = new Document.OutputSettings
 		outputSettings.prettyPrint(false)
 		doc.outputSettings(outputSettings)
-		doc.select("br").before("\\r\\n\\t")
-		doc.select("p").append("\\r\\n\\t")
+		doc.select("br").before("\\n\\n")
+		doc.select("p").append("\\n\\n")
 
 		val header = doc.select(".news-title").first().text()
-		val content = doc.select(".news-text").first().text()
+		val content = doc.select(".news-text").first().text().replaceAll("\\\\n", "\n")
 		val date = doc.select(".news-date > span").first().text()
 
 		NewsDTO(header, content, date)
